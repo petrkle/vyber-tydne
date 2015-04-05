33aea3fd5b0ef5de55193499fd61db207042d47c
Petr <petr@kle.cz>
2015-04-05 22:58:14 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,22 @@
+activate :livereload, :no_swf => true
+
+Time.zone = "Europe/Prague"
+
+activate :blog do |blog|
+	blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "stranka/{num}"
+	blog.permalink = ":year-:month-:day.html"
+end
+
+page "/feed.xml", layout: false
+set :css_dir, 'css'
+set :images_dir, 'img'
+
+# Build-specific configuration
+configure :build do
+	activate :minify_html
+  activate :minify_css
+  activate :asset_hash
+  activate :relative_assets
+end
